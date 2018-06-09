153c0c18ea02331f2d40678b0dc0cbd6e6492ca9
Jyoti <jyotisingla8611@gmail.com>
2018-06-09 14:33:46 +0530

    My Third Commit

--- Pattern.java
+++ Pattern.java
@@ -7,7 +7,7 @@ public class Pattern {
 		{
 			for(int j=0;j<i;j++)
 			{
-				System.out.print(j);		
+				System.out.print("*");		
 			}
 			System.out.println();
 		}
@@ -15,7 +15,7 @@ public class Pattern {
 		{
 			for(int j=0;j<i;j++)
 			{
-				System.out.print(j);		
+				System.out.print("*");		
 			}
 			System.out.println();
 		}
